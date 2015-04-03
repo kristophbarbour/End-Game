75b31d71c25cb9d6155cac9abc73712251e44ab8
kristophbarbour <kristophbarbour@gmail.com>
2015-04-04 00:04:48 +1100

    Initial Character Creation
    Added in robot Kyle and basic Third Person movement and camera

---
+++ End Game/Assets/Scripts/CharacterMotor.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMotor : MonoBehaviour
+{
+
+	private float h;
+	private float v;
+
+	private float mx;
+	private float my;
+
+	private CharacterController cc;
+	private Animator anim;
+	
+	private Vector3 moveDirection;
+	private Vector3 targetDirection;
+
+	[SerializeField]
+	private float
+		moveSpeed = 5f;
+	[SerializeField]
+	private float
+		turnSpeed = 5f;
+	[SerializeField]
+	private float
+		jumpHeight = 8f;
+	[SerializeField]
+	private float
+		gravity = 20f;
+
+	private float vSpeed = 0f;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		cc = GetComponent<CharacterController> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per Pysics update
+	void FixedUpdate ()
+	{
+		// Get movement input
+		h = Input.GetAxis ("Horizontal");
+		v = Input.GetAxis ("Vertical");
+		// Get mouse input
+		mx = Input.GetAxis ("Mouse X");
+		my = Input.GetAxis ("Mouse Y");
+
+		// Get Characters forward position
+		Vector3 forward = transform.TransformDirection (Vector3.forward);
+		forward.y = 0;
+		// Get Characters right position
+		Vector3 right = transform.TransformDirection (Vector3.right);
+
+		// Check for Jumping and apply gravity
+		if (cc.isGrounded) {
+			vSpeed = 0f;
+			if (Input.GetButtonDown ("Jump")) {
+				vSpeed = jumpHeight;
+			}
+		}
+		
+		// Move Character
+		targetDirection = (h * right) + (v * forward); // Convert to int
+		moveDirection = targetDirection.normalized * moveSpeed;
+		// Add jumping
+		vSpeed -= gravity * Time.deltaTime;
+		moveDirection.y = vSpeed;
+		// Move Character if there is input
+		if (!Idle ()) {
+			cc.Move (moveDirection * Time.deltaTime);
+		}
+
+		RotateCharacter ();
+
+	}
+
+	public bool Idle ()
+	{
+		return Input.GetAxisRaw ("Horizontal") == 0 && Input.GetAxisRaw ("Vertical") == 0 && vSpeed == 0;
+	}
+
+	void RotateCharacter ()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+
+		float rotY = 0f;
+		rotY += mx * turnSpeed;
+
+		transform.Rotate (0, rotY, 0);
+
+
+	}
+}
---
+++ End Game/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+
+	[SerializeField]
+	private float
+		sensitivityX = 15F;
+	[SerializeField]
+	private float
+		sensitivityY = 15F;
+
+	[SerializeField]
+	private float
+		minimumY = -60F;
+	[SerializeField]
+	private float
+		maximumY = 20F;
+
+	float rotationX = 0F;
+	float rotationY = 0F;
+
+	Quaternion originalRotation;
+
+	void Update ()
+	{
+		rotationY += Input.GetAxis ("Mouse Y") * sensitivityY;
+		rotationY = ClampAngle (rotationY, minimumY, maximumY);
+		
+		Quaternion yQuaternion = Quaternion.AxisAngle (Vector3.left, Mathf.Deg2Rad * rotationY);
+		transform.localRotation = originalRotation * yQuaternion;
+	}
+	
+	void Start ()
+	{
+		// Make the rigid body not change rotation
+		if (GetComponent<Rigidbody> ())
+			GetComponent<Rigidbody> ().freezeRotation = true;
+		originalRotation = transform.localRotation;
+	}
+
+	public static float ClampAngle (float angle, float min, float max)
+	{
+		if (angle < -360F)
+			angle += 360F;
+		if (angle > 360F)
+			angle -= 360F;
+		return Mathf.Clamp (angle, min, max);
+	}
+}
