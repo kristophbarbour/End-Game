4b3dcbbb3f9856eeb30bf3f10b542b19e0e3f3e2
gresc574 <scottgreen1991@hotmail.com>
2015-04-07 21:07:51 +1200

    basic IK none of it works really

---
+++ End Game/Assets/HoldWeapon.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using RootMotion.FinalIK;
+
+public class HoldWeapon : MonoBehaviour
+{
+	
+	[SerializeField]
+	FullBodyBipedIK
+		ik;
+	[SerializeField]
+	Transform
+		aimTarget;
+	[SerializeField]
+	Transform
+		leftHandTarget, rightHandTarget;
+	
+	
+	void LateUpdate ()
+	{
+		ik.solver.leftHandEffector.position = leftHandTarget.position;
+		ik.solver.leftHandEffector.rotation = leftHandTarget.rotation;
+		ik.solver.leftHandEffector.positionWeight = 1f;
+		ik.solver.leftHandEffector.rotationWeight = 1f;
+		
+		ik.solver.rightHandEffector.position = rightHandTarget.position;
+		ik.solver.rightHandEffector.rotation = rightHandTarget.rotation;
+		ik.solver.rightHandEffector.positionWeight = 1f;
+		ik.solver.rightHandEffector.rotationWeight = 1f;
+		
+		transform.LookAt (aimTarget.position);
+	}
+}
